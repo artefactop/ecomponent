77c4d795fc308508f431505804beb629a02012b2
Pepe <pepe@yuilop.com>
2012-09-25 16:41:53 +0200

    One fix more, function name change

--- src/iq_handler.erl
+++ src/iq_handler.erl
@@ -37,7 +37,7 @@ process_iq(#params{type="set", ns=NS, iq=IQ, from=From}=Params) ->
             forward_ns(Params);
         false ->
             ecomponent:syslog(warning, io_lib:format("Access denied for: ~p to ~p~n", [From, NS])),
-            ecomponent:call_send(exmpp_iq:error(IQ, 'bad-request'), NS, undefined)
+            ecomponent:send(exmpp_iq:error(IQ, 'bad-request'), NS, undefined)
     end;
 
 process_iq(#params{type="get", ns=NS, iq=IQ, from=From}=Params) ->
@@ -46,7 +46,7 @@ process_iq(#params{type="get", ns=NS, iq=IQ, from=From}=Params) ->
             forward_ns(Params);
         false ->
             ecomponent:syslog(warning, io_lib:format("Access denied for: ~p to ~p~n", [From, NS])),
-            ecomponent:call_send(exmpp_iq:error(IQ, 'bad-request'), NS, undefined)
+            ecomponent:send(exmpp_iq:error(IQ, 'bad-request'), NS, undefined)
     end;
 
 process_iq(P) ->
